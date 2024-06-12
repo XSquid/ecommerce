12410a1594e3fa346dc114eca261c164b2ecc48d
XSquid <102339058+XSquid@users.noreply.github.com>
2024-06-12 00:33:59 -0600

    Set up basic express and connected to empty database

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const db = require ('./queries')
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', db.getUsers)
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// user for ecommerce db username = 'ecommerce' password = 'password'
---
+++ queries.js
@@ -0,0 +1,14 @@
+const db = require('./database');
+
+const getUsers = (request, response) => {
+    db.pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getUsers
+}
